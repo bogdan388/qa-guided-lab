60f45e42e9b0164d217f156ae507d07b4b059568
Bogdan Iordache <bogdan388@gmail.com>
2025-10-13 12:12:25 +0300

    feat: salutare mult mai expresiva

--- App.java
+++ App.java
@@ -1,10 +1,14 @@
 public class App {
+    // Versiunea 2.0
     public void salut(String nume) {
-        System.out.println("Salut! " + nume + "!");
+        System.out.println("======================");
+        System.out.println("SALUTARI DIN ROMANIA!");
+        System.out.println("Buna " + nume + "!");
+        System.out.println("======================");
     }
 
     public static void main(String[] args) {
         App app = new App();
-        app.salut("Utilizator");
+        app.salut("Dragul meu utilizator");
     }
 }
