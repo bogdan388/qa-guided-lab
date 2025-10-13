8d0cfb8fdac0e4ac130da2af403876d0113aace7
Bogdan Iordache <bogdan388@gmail.com>
2025-10-13 12:02:57 +0300

    feat: adauga camp mesaj personalizat si intrebare suplimentara

--- App.java
+++ App.java
@@ -1,10 +1,13 @@
 public class App {
+    private String mesajSalut = "Bine ai venit";
+
     public void salut(String nume) {
-        System.out.println("Salut! " + nume + "!");
+        System.out.println(mesajSalut + ", " + nume + "!");
+        System.out.println("Cum te mai simti azi?");
     }
 
     public static void main(String[] args) {
         App app = new App();
-        app.salut("Utilizator");
+        app.salut("Prietene");
     }
 }
