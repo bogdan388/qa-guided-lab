1dd6ece0576b192f065f9972df20882be5c722e6
Bogdan Iordache <bogdan388@gmail.com>
2025-10-13 12:05:33 +0300

    feat: adauga metoda salutFormal si mesaj suplimentar

--- App.java
+++ App.java
@@ -1,10 +1,17 @@
 public class App {
+    // Aplicatie de salutare
     public void salut(String nume) {
-        System.out.println("Salut! " + nume + "!");
+        System.out.println("Salutare, " + nume + "!");
+        System.out.println("O zi minunata iti doresc!");
+    }
+
+    public void salutFormal(String nume, String titlu) {
+        System.out.println("Buna ziua, " + titlu + " " + nume);
     }
 
     public static void main(String[] args) {
         App app = new App();
-        app.salut("Utilizator");
+        app.salut("Coleg");
+        app.salutFormal("Popescu", "Domnule");
     }
 }
